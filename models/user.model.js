af328afa8c0418621637f52fe04f815eb70aa147
Iordanis Papaditsas <114758586+jordanpapaditsas@users.noreply.github.com>
2023-09-15 10:54:58 +0300

    initial commit

---
+++ models/user.model.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+const Schema = mongoose.Schema;
+
+let userSchema = new Schema({
+  username: {
+    type: String,
+    required: [true, 'Username is required field'],
+    max: 15,
+    unique: true,
+    trim: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required field'],
+    min: 3,
+    max: 15,
+  },
+  name: { type: String },
+  surname: { type: String },
+  email: {
+    type: String,
+    required: [true, 'Email is required field'],
+    max: 20,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 
+            'Email address is not valid'],
+  },
+  address: addressSchema,
+  phone: { type: [phoneSchema], null: true },
+  products: { type: [productSchema], null: true}
+},
+{
+  collection: 'users',
+  timestamps: true
+});
+
+let addressSchema = new Schema({
+  area: { type: String },
+  road: { type: String },
+}, { _id: false });
+
+let phoneSchema = new Schema({
+  type: { type: String },
+  number: { type: String }
+}, { _id: false });
+
+let productSchema = new Schema({
+  product: { type: String },
+  cost: { type: Number },
+  quantity: { type: Number, required: true },
+  date: { type: Date, default: Date.now },
+});
+
+userSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model('User', userSchema);
